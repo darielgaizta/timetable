70c8410c61e7d7d20147d473721326048a999977
darielgaizta <darielgaz@gmail.com>
2024-07-31 12:19:30 +0700

    add ui for step 2

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('1/', views.step_1, name='step_1'),
+    path('2/', views.step_2, name='step_2'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-def index(request):
+def step_1(request):
+    if request.method == 'POST':
+        return redirect(step_2)
     return render(request, 'views/step_1.html')
+
+def step_2(request):
+    if request.method == 'POST':
+        if request.POST.get('previous', 0): return redirect(step_1)
+    return render(request, 'views/step_2.html')
